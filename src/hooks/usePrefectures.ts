af2676d859513eaa7580e9b78d470a93064aa758
XPA <xpadev@gmail.com>
2024-05-04 19:19:41 +0900

    feat: add cache to services

--- src/hooks/usePrefectures.ts
+++ src/hooks/usePrefectures.ts
@@ -8,15 +8,15 @@ export const usePrefectures = () => {
   const [data, setData] = useState<TApiResponse<TPrefectureResponse>>();
   const [loading, setLoading] = useState<boolean>(false);
 
-  const fetchData = async () => {
+  const fetchData = async (forceUpdate = true) => {
     setLoading(true);
-    const response = await getPrefectures();
+    const response = await getPrefectures(forceUpdate);
     setData(response);
     setLoading(false);
   };
 
   useEffect(() => {
-    void fetchData();
+    void fetchData(false);
   }, []);
   return { data, loading, refetch: fetchData };
 };
--- src/services/getPopulationCompositionPerYear.ts
+++ src/services/getPopulationCompositionPerYear.ts
@@ -6,19 +6,32 @@ import {
 import { TApiResponse } from "@/@types/api/response";
 import { requests } from "@/lib/requests.ts";
 
+const cache: Record<number, TPopulationCompositionResponse> = {};
+
 export const getPopulationCompositionPerYear = async (
   prefCode: number,
+  forceUpdate = false,
 ): Promise<TApiResponse<TPopulationCompositionResponse>> => {
+  const cacheItem = cache[prefCode];
+  if (!forceUpdate && cacheItem) {
+    return {
+      type: "success",
+      data: cacheItem,
+    };
+  }
+
   const data = await requests(
     `/population/composition/perYear?prefCode=${prefCode}&cityCode=-`,
   );
   const result = ZPopulationCompositionResponse.safeParse(data);
   if (result.success) {
+    cache[prefCode] = result.data;
     return {
       type: "success",
       data: result.data,
     };
   }
+  delete cache[prefCode];
   const error = ZErrorResponse.safeParse(data);
   if (error.success) {
     return {

e20f247d11b1aa30374dfa8ad457da6e2dcaac6e
XPA <xpadev@gmail.com>
2024-05-04 19:20:36 +0900

    fix: change to use useCallback

--- src/hooks/usePrefectures.ts
+++ src/hooks/usePrefectures.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { TPrefectureResponse } from "@/@types/api/prefectures.ts";
 import { TApiResponse } from "@/@types/api/response";
@@ -8,15 +8,15 @@ export const usePrefectures = () => {
   const [data, setData] = useState<TApiResponse<TPrefectureResponse>>();
   const [loading, setLoading] = useState<boolean>(false);
 
-  const fetchData = async (forceUpdate = true) => {
+  const fetchData = useCallback(async (forceUpdate = true) => {
     setLoading(true);
     const response = await getPrefectures(forceUpdate);
     setData(response);
     setLoading(false);
-  };
+  }, []);
 
   useEffect(() => {
     void fetchData(false);
-  }, []);
+  }, [fetchData]);
   return { data, loading, refetch: fetchData };
 };
